ecba03b2f2387dd52a5e869b93ef1e91cd09ddfe
Luke <luke.g.schwade@gmail.com>
2023-11-27 19:39:23 -0500

    added night mode with js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// //Check if Nightmode box is clicked using querySelector
+
+// const checkbox = document.querySelector("input[name='nightCheck']",
+// );
+// console.log(checkbox);
+// console.log("hello");
+// checkbox.addEventListener('change', function() {
+//     const element = document.getElementById("mainContent");
+//   if (this.checked) {
+//     element.classList.toggle("night-mode")
+//     console.log("Checkbox is checked..");
+//   } else {
+//     console.log("Checkbox is not checked..");
+//     element.classList.toggle("night-mode")
+
+//   }
+// });
+
+// ATTEMPT #2 check if checkbox element is clicked on
+
+function toggleNightMode() {
+    const element = document.getElementById ("mainContent");
+    const element2 = document.getElementById("quote");
+    element.classList.toggle("night-mode");
+    console.log(element);
+    console.log(element2);
+    element2.classList.toggle("night-mode");
+}
